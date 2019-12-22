d95b9ebbb377a1646d2ba4eeb45b9d4f8e54fb21
Miguel Ruiz <mruiz@auvik.com>
2019-12-22 22:07:56 +0100

    Add first links for basic set

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Flex, Box } from 'reflexbox'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
 import basicImg from "../images/basic.jpg"
 import gamerImg from "../images/gamer-office.jpg"
 import homeImg from "../images/home-office.jpg"
@@ -12,20 +14,31 @@ const IndexPage = () => (
     <SEO title="Lista de sets" />
     <h1>Sets predefinidos</h1>
     <p>Todo lo que necesitas para estudiar, trabajar o divertirte con consejos de profesionales</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    <Link to={'/sets/basicset'}>
-      <img src={basicImg} alt="Basic"/>
-    </Link>
 
-    <Link to={'/sets/officeset'}>
-      <img src={homeImg} alt="Basic"/>
-    </Link>
+     <Flex flexWrap='wrap'>
+      <Box
+        width={[ 1, 1/3 ]}
+        p={3}>
+        <Link to={'/sets/basicset'}>
+          <img src={basicImg} alt="Basic"/>
+        </Link>
+      </Box>
+      <Box
+        width={[ 1, 1/3 ]}
+        p={3}>
+        <Link to={'/sets/officeset'}>
+          <img src={homeImg} alt="Basic"/>
+        </Link>
+      </Box>
+      <Box
+        width={[ 1, 1/3 ]}
+        p={3}>
+        <Link to={'/sets/gamerset'}>
+          <img src={gamerImg} alt="Basic"/>
+        </Link>
+      </Box>
+    </Flex>
 
-    <Link to={'/sets/gamerset'}>
-      <img src={gamerImg} alt="Basic"/>
-    </Link>
-
-    </div>
   </Layout>
 )
 
--- src/pages/sets/basicset.js
+++ src/pages/sets/basicset.js
@@ -3,12 +3,38 @@ import React from "react"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 
-export default () => (
-  <Layout>
-    <SEO title="Set escritorio básico" />
-    <h1>Set básico</h1>
-    <p>Lo mínimo que se necesita para montar una escritorio en casa</p>
+var links = [
+  'https://rcm-eu.amazon-adsystem.com/e/cm?ref=tf_til&t=trabajoremoto-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B012C0SG6U&linkId=38de765d490abbd7075978f14aac2ebb&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr',
+  'https://rcm-eu.amazon-adsystem.com/e/cm?ref=tf_til&t=trabajoremoto-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B073NTCT4R&linkId=d7b0ccadd73eced86874ab7852d58001&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr',
+  'https://rcm-eu.amazon-adsystem.com/e/cm?ref=tf_til&t=trabajoremoto-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B0178A0KU4&linkId=5d8d58ca45605c7a5fd01414d89e6a73&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr',
+  'https://rcm-eu.amazon-adsystem.com/e/cm?ref=tf_til&t=trabajoremoto-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B0178A0KU4&linkId=5d8d58ca45605c7a5fd01414d89e6a73&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr',
+  'https://rcm-eu.amazon-adsystem.com/e/cm?ref=tf_til&t=trabajoremoto-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B06X3W3YQD&linkId=d02a29efec559a77565641ac239d2242&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr',
+  'https://rcm-eu.amazon-adsystem.com/e/cm?ref=tf_til&t=trabajoremoto-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B07H3PJHZ8&linkId=04dbfb15b75c00e342ad6095f4073b0b&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr',
+  '',
+]
 
+
+const BasicPage = () => (
+  <Layout>
+  <SEO title="Set escritorio básico" />
+  <h1>Set básico</h1>
+  <p>Lo mínimo que se necesita para montar una escritorio en casa</p>
+
+  {
+  links.map((link) =>
+    <iframe style={{
+          width: `120px`,
+          height: '240px',
+        }}
+        marginWidth={0}
+        marginHeight={0}
+        scrolling={"no"}
+        frameBorder={0} src={link}
+        >
+      </iframe>)
+  }
 
   </Layout>
 )
+
+export default BasicPage
