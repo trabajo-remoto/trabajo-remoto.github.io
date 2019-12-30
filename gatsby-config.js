c23a3d9fd89ff39dc02a3b04e7eccabef154d8a0
Miguel Ruiz <mruiz@auvik.com>
2019-12-30 17:44:28 +0100

    Small refactor. Place things

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/src/posts`,
+        path: `${__dirname}/src/blog/posts`,
       },
     },
     {
