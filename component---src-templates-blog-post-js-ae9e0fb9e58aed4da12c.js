1d6a1a7cfc41e968a8448989825766ed435d33e8
Miguel Ruiz <mruiz@auvik.com>
2020-01-04 17:13:25 +0100

    Updates

---
+++ component---src-templates-blog-post-js-ae9e0fb9e58aed4da12c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{yZlL:function(e,t,n){"use strict";n.r(t),n.d(t,"query",(function(){return u}));var a=n("q1tI"),r=n.n(a),l=n("Bl7J");t.default=function(e){var t=e.data.markdownRemark;return r.a.createElement(l.a,null,r.a.createElement("div",null,r.a.createElement("h1",null,t.frontmatter.title),r.a.createElement("div",{dangerouslySetInnerHTML:{__html:t.html}})))};var u="861893041"}}]);
+//# sourceMappingURL=component---src-templates-blog-post-js-ae9e0fb9e58aed4da12c.js.map
